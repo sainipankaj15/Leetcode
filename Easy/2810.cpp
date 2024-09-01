9e0db329e39255e497a54e06f128222c1dadfa9c
sainipankaj15 <pksmks1999@gmail.com>
2024-09-01 15:45:49 +0530

    01-09-2024

---
+++ Easy/2810.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string finalString(string s) {
+        
+        string ans="";
+        
+        for(auto i : s){
+            if(i=='i'){
+                reverse(ans.begin(),ans.end());
+            }
+            else
+                ans+=i;
+        }
+        return ans;
+        
+    }
+};
